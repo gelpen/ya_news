6c89654d4bbf65ac34fc33b3af09fe1e146d0b24
Yevgeniy_T <59078119+gelpen@users.noreply.github.com>
2025-01-13 17:47:24 +0500

    ok

--- news/pytest_trial/test_example.py
+++ news/pytest_trial/test_example.py
@@ -1,11 +1,24 @@
 # test_example.py
-def one_more(x):
-    return x + 1
+# def one_more(x):
+#     return x + 1
 
 
-def test_correct():
-    assert one_more(4) == 5
+# def test_correct():
+#     assert one_more(4) == 5
 
 
-def test_fail():
-    assert one_more(3) == 5
+# def test_fail():
+#     # assert one_more(3) == 5
+#     assert one_more(4) == 5
+
+
+def get_sort_list(string):
+    new_list = sorted(string.split(', '))
+    return new_list
+
+
+def test_sort():
+    """Тестируем функцию get_sort_list()."""
+    result = get_sort_list('Яша, Саша, Маша, Даша')
+    assert result == ['Даша', 'Маша', 'Саша', 'Яша']
+    # assert result == ['Яша', ' Саша', ' Маша', ' Даша']
