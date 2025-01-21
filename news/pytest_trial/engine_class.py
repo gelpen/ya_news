99f68451e26c2f1a09cf93efdc02f47470677ee5
Yevgeniy_T <59078119+gelpen@users.noreply.github.com>
2025-01-21 18:15:35 +0500

    ok 21012025 1815

---
+++ news/pytest_trial/engine_class.py
@@ -0,0 +1,9 @@
+
+
+# engine_class.py
+class Engine:
+    """Класс двигателя."""
+    
+    def __init__(self):
+        # При создании объекта двигателя он не запущен.
+        self.is_running = False
---
+++ news/pytest_trial/test_engine.py
@@ -0,0 +1,26 @@
+
+# test_engine.py
+import pytest
+
+# Импортируем класс двигателя.
+from engine_class import Engine
+
+
+@pytest.fixture
+def engine():
+    """Фикстура возвращает экземпляр класса двигателя."""
+    return Engine()
+
+
+# Эта фикстура не возвращает никаких значений, но изменяет объект,
+# созданный другой фикстурой.
+@pytest.fixture
+def start_engine(engine):  # Вызываем фикстуру получения объекта двигателя.
+    """Фикстура запускает двигатель."""
+    # Изменяем значение свойства объекта:
+    engine.is_running = True
+
+
+def test_engine_is_running(engine, start_engine):  # Вызываем обе фикстуры.
+    """Тест проверяет, работает ли двигатель."""
+    assert engine.is_running  # Проверяем, что значение атрибута is_running это True.
